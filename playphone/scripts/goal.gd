0f28e40eaec82c2165c7f950f8c25331356d87f3
Avocado <hamzayabdi@gmail.com>
2025-10-11 13:25:58 -0400

    scene stuff
    fixed all the shit i messed up with the main menu and added a controls and end screen, making it so that when you touch the goal (estrogen) you get sent to the end screen

--- playphone/scripts/goal.gd
+++ playphone/scripts/goal.gd
@@ -7,8 +7,7 @@ func _ready() -> void:
 	connect("body_exited", _on_body_exited)
 var b = null
 func _on_body_entered(body):
-	if body is CharacterBody2D:
-		b = body
+	get_tree().change_scene_to_file("res://Scenes/end.tscn")
 func _on_body_exited(body):
 	if body is CharacterBody2D:
 		b = null
