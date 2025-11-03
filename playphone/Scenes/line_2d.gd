f99fae67a4ae6b9cc2045e70045535ab7b25a71b
ShyMike <122023566+ImShyMike@users.noreply.github.com>
2025-11-03 17:19:46 +0000

    add silly dash trail

---
+++ playphone/Scenes/line_2d.gd
@@ -0,0 +1,17 @@
+extends Line2D
+
+@export var maxPoints: int = 50
+@export var enabled: bool = true
+var currentMaxPoints: float = 0
+
+func _ready() -> void:
+	top_level = true
+
+func _process(_delta: float) -> void:
+	var target = maxPoints if enabled else 0
+	currentMaxPoints = move_toward(currentMaxPoints, target, 1)
+
+	add_point(get_parent().global_position)
+
+	while get_point_count() > currentMaxPoints:
+		remove_point(0)
--- playphone/scripts/player.gd
+++ playphone/scripts/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-@export var maxHorisontalSpeed: float = 300.0;
 @export var boostStrength: float = 700.0;
 @export var coyoteTime: float = 0.2;
 const SPEED = 300.0;
@@ -10,11 +9,12 @@ const DEATH_Y = 100;
 var coyoteTimer = coyoteTime;
 var dashCount = 1;
 var extraJumps = 1;
-var accelX = 0;
-
+var accel: Vector2 = Vector2.ZERO;
+@onready var line2d = $Line2D;
 
 func death():
 	var spawn = get_parent().get_child(2).get_child(0).get_node("SpawnPoint")
+	line2d.clear_points()
 	if spawn:
 		global_position = spawn.global_position
 
@@ -27,7 +27,7 @@ func _physics_process(delta: float) -> void:
 		velocity.y += get_gravity().y * delta
 
 	if Input.is_action_just_pressed("dash") and (dashCount > 0 and direction != 0):
-		accelX = DASH_VELOCITY * direction
+		accel.x = DASH_VELOCITY * direction
 		if dashCount > 0:
 			dashCount -= 1
 
@@ -45,7 +45,7 @@ func _physics_process(delta: float) -> void:
 		coyoteTimer -= delta
 
 	if direction != 0:
-		velocity.x = direction * SPEED + accelX
+		velocity.x = direction * SPEED + accel.x
 		$AnimatedSprite2D.flip_h = direction < 0
 		if not $AnimatedSprite2D.is_playing():
 			$AnimatedSprite2D.play("walk")
@@ -54,12 +54,14 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite2D.stop()
 
 	if is_on_floor():
-		accelX = move_toward(accelX, 0, 50)
+		accel.x = move_toward(accel.x, 0, 50)
 	else:
-		accelX = move_toward(accelX, 0, 10)
+		accel.x = move_toward(accel.x, 0, 10)
 
 	if position.y >= DEATH_Y:
 		death()
+	
+	line2d.enabled = abs(velocity.x) > SPEED
 
 	move_and_slide()
 
